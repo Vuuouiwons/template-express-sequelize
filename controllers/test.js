ff7379b1bd09de0d7414d511789238fb0ed82633
Vuuou!wons <vinconloren@gmail.com>
2023-03-07 10:27:07 +0700

    init template complete

--- controllers/test.js
+++ controllers/test.js
@@ -7,38 +7,57 @@ const { payload } = require("../templates/payloadContructor");
 
 module.exports = {
 	postData: async (req, res) => {
-		const body = req.body;
+		try {
+			const body = req.body;
 
-		const test = await Test.create({ data: JSON.stringify(body.data) });
+			const test = await Test.create({ data: JSON.stringify(body.data) });
 
-		return res.status(201).send(payload(201, "data uploaded", body.data));
+			return res.status(201).send(payload(201, "data uploaded", body.data));
+		} catch (err) {
+			return res.status(500).send(payload(500, err, null));
+		}
 	},
 	getData: async (req, res) => {
-		const data = await Test.findAll();
+		try {
+			const data = await Test.findAll();
 
-		return res.status(200).send(payload(200, "data succesfully grabbed", data));
+			return res
+				.status(200)
+				.send(payload(200, "data succesfully grabbed", data));
+		} catch (err) {
+			return res.status(500).send(payload(500, "internal server error", null));
+		}
 	},
 	updateData: async (req, res) => {
-		const body = req.body;
+		try {
+			const body = req.body;
 
-		const test = await Test.update(
-			{ data: body.data },
-			{
-				where: {
-					id: body.id,
-				},
-			}
-		);
-
-		return res
-			.status(201)
-			.send(payload(201, "data successfully updated", body.data));
+			const test = await Test.update(
+				{ data: body.data },
+				{
+					where: {
+						id: body.id,
+					},
+				}
+			);
+			return res
+				.status(201)
+				.send(payload(201, "data successfully updated", body.data));
+		} catch (err) {
+			return res.status(500).send(payload(500, "internal server error", null));
+		}
 	},
 	deleteData: async (req, res) => {
-		const body = req.body;
+		try {
+			const body = req.body;
 
-		const test = await Test.destroy({ where: { id: body.id } });
+			const test = await Test.destroy({ where: { id: body.id } });
 
-		return res.status(200).send(payload(200, "data succesfully deleted", null));
+			return res
+				.status(200)
+				.send(payload(200, "data succesfully deleted", null));
+		} catch (err) {
+			return res.status(500).send(payload(500, "internal server error", null));
+		}
 	},
 };
--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 require("dotenv").config();
 
 const express = require("express");
+const morgan = require("morgan");
 const app = express();
 const allRouter = require("./routers");
+app.use(morgan("combined"));
 
 app.use("/api", allRouter);
 
